692a20d376397ca11e2ec92271157c931d076701
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-03 01:04:26 +0000

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,4 @@
-& mvn -f .\client\pom.xml -B -Psign-artifacts -DskipTests clean verify org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish
+$env:GPGEXE = "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
+$env:GNUPGHOME = "C:\Users\adm101\AppData\Roaming\gnupg"
+
+& mvn -f .\client\pom.xml -B -Psign-artifacts -DskipTests -e clean verify org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish

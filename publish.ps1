f2799bf66d9ad7f038779fda08375999d7ecb4aa
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-03 00:40:12 +0000

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,1 @@
-mvn -B -Psign-artifacts -DskipTests clean package org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish
+& mvn -f .\client\pom.xml -B -Psign-artifacts -DskipTests clean verify org.sonatype.central:central-publishing-maven-plugin:0.9.0:publish
